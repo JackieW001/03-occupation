20bf4d9b2f5af080f0ce75336aac399053d81aaf
henryz2000 <hzheng3@stuy.edu>
2017-09-15 13:27:35 -0400

    update 2

--- work03.py
+++ work03.py
@@ -5,8 +5,17 @@ csvfile = f.read()
 csvfile = csvfile.split("\r\n")
 csvfile = csvfile[1:-2]
 
-print csvfile
 f.close()
+
+dictionary = {}
+
+for line in csvfile:
+    innerdict = line.split(",")
+    #print innerdict
+    dictionary[innerdict[0]] = innerdict[1]
+
+#print csvfile[0]
+print dictionary
 
 
 """
